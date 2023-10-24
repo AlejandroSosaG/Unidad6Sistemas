dfdb6d82c590bad7714c617940f68c8178a713c6
AlejandroSosaG <alejandro.sosa@iesnervion.es>
2023-10-24 18:32:10 +0200

    Agregar archivos de proyecto.

---
+++ Ejercicio4/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Ejercicio4.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Ejercicio4.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PrimeraAplicacion/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrimeraAplicacion.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public String Apellidos()
+        {
+            return "Sosa García";
+        }
+        public IActionResult Saludo()
+        {
+            return View();
+        }
+    }
+}
